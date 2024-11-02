eb5db8605fac502595eb1591ca97aadac939b944
MaxiGomez5431 <mamitgomez@gmail.com>
2024-11-02 00:35:50 -0300

    Header style added

---
+++ src/components/interface/Header.jsx
@@ -0,0 +1,19 @@
+import netflixLogo from '../../assets/netflixLogo.png'
+
+  const iconStyle = "flex justify-center items-center text-white m-1 w-10 h-10 duration-100 hover:bg-gray-500"
+
+export function Header () {
+  return (
+    <header className='flex justify-between bg-black'>
+      <div className="flex justify-center items-center">
+        <i className={`fa-solid fa-bars fa-xl ${iconStyle}`}/>
+        <img className="w-28" src={netflixLogo}/>
+      </div>
+      
+      <div className="flex justify-center items-center">
+        <i className={`fa-solid fa-magnifying-glass fa-xl ${iconStyle}`}/> 
+        <i className={`fa-solid fa-ellipsis fa-xl ${iconStyle}`}/>
+      </div>
+  </header>
+  )
+}
